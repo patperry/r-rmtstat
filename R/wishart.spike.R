06011e17e8d7774af55333aab4cbae1e46ffe8ec
Patrick Perry <patperry@stanford.edu>
2009-04-29 12:57:26 -0700

    add beta parameter for spiked wishart

--- R/wishart.spike.R
+++ R/wishart.spike.R
@@ -1,18 +1,18 @@
 
-wishart.spike.par <- function( spike, n.df=NA, p.dim=NA, var=1 ) {
+wishart.spike.par <- function( spike, n.df=NA, p.dim=NA, var=1, beta=1 ) {
     ratio  <- p.dim/n.df
     above  <- spike > sqrt( ratio )*var 
     center <- ifelse( !above, NA,
                   ( spike + var )*( 1 + ratio*( var/spike ) ) )
     scale  <- ifelse( !above, NA,
                   ( ( spike + var )
-                    * sqrt( 2*( 1 - ratio*( var/spike )^2 ) ) 
+                    * sqrt( (2/beta)*( 1 - ratio*( var/spike )^2 ) ) 
                     / sqrt( n.df ) ) )
     
     list( center=center, scale=scale )
 }
 
-wishart.spike.vec.par <- function( spike, n.df=NA, p.dim=NA, var=1 ) {
+wishart.spike.vec.par <- function( spike, n.df=NA, p.dim=NA, var=1, beta=1 ) {
     ratio <- p.dim/n.df
     above <- spike > sqrt( ratio )*var 
     cor2  <- ifelse( !above, NA,
@@ -23,29 +23,29 @@ wishart.spike.vec.par <- function( spike, n.df=NA, p.dim=NA, var=1 ) {
     cor2
 }
 
-dwishart.spike <- function( x, spike, n.df=NA, p.dim=NA, var=1, 
+dwishart.spike <- function( x, spike, n.df=NA, p.dim=NA, var=1, beta=1,
                             log = FALSE ) {
-    params <- wishart.spike.par( spike, n.df, p.dim, var )
+    params <- wishart.spike.par( spike, n.df, p.dim, var, beta )
     d      <- dnorm( x, mean=params$center, sd=params$scale, log=log )
     d
 }
 
-pwishart.spike <- function( q, spike, n.df=NA, p.dim=NA, var=1, 
+pwishart.spike <- function( q, spike, n.df=NA, p.dim=NA, var=1, beta=1,
                             lower.tail = TRUE, log.p = FALSE ) {
-    params <- wishart.spike.par( spike, n.df, p.dim, var )
+    params <- wishart.spike.par( spike, n.df, p.dim, var, beta )
     p      <- pnorm( q, mean=params$center, sd=params$scale, lower.tail, log.p )
     p
 }                                
 
-qwishart.spike <- function( p, spike, n.df=NA, p.dim=NA, var=1, 
+qwishart.spike <- function( p, spike, n.df=NA, p.dim=NA, var=1, beta=1,
                             lower.tail = TRUE, log.p = FALSE ) {
-    params <- wishart.spike.par( spike, n.df, p.dim, var )
+    params <- wishart.spike.par( spike, n.df, p.dim, var, beta )
     q      <- qnorm( q, mean=params$center, sd=params$scale, lower.tail, log.p )
     q
 }                                
 
-rwishart.spike <- function( n, spike, n.df=NA, p.dim=NA, var=1 ) {
-    params <- wishart.spike.par( spike, n.df, p.dim, var )
+rwishart.spike <- function( n, spike, n.df=NA, p.dim=NA, var=1, beta=1 ) {
+    params <- wishart.spike.par( spike, n.df, p.dim, var, beta )
     x      <- qnorm( n, mean=params$center, sd=params$scale )
     x
 }
