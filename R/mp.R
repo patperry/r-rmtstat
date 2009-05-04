3841b506ff30375e83ba2b02cf621b8475aceca5
Patrick Perry <patperry@stanford.edu>
2009-05-04 15:32:58 -0700

    fix bugs in dmp

--- R/mp.R
+++ R/mp.R
@@ -6,21 +6,23 @@ dmp <- function( x, n.df=NA, p.dim=NA, var=1, concentration=n.df/p.dim, log = FA
     b              <- var*( 1 + inv.gamma.sqrt )^2
     
     if( !log ) {
-        d <- ifelse( x <= a || x >= b, 0,
-                gamma/( 2*pi*var*x )
-                *
-                sqrt( ( x-a )*( b-x ) ) 
-                )
+        d <- ifelse( x <= a | x >= b, 0,
+                suppressWarnings(
+                    gamma/( 2*pi*var*x )
+                    *
+                    sqrt( ( x-a )*( b-x ) ) 
+                    ) )
     } else {
-        d <- ifelse( x <= a || x >= b, -Inf,
-                 log( gamma ) 
-                 - 
-                 ( log( 2 ) + log( pi ) + log( var ) + log( x ) )
-                 +
-                 0.5*log( x-a ) 
-                 + 
-                 0.5*log( b-x )
-                 )
+        d <- ifelse( x <= a | x >= b, -Inf,
+                 suppressWarnings(
+                     log( gamma ) 
+                     - 
+                     ( log( 2 ) + log( pi ) + log( var ) + log( x ) )
+                     +
+                     0.5*log( x-a ) 
+                     + 
+                     0.5*log( b-x )
+                     ) )
     }
     d
 }
