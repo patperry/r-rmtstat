36edd53a81953b9260a926528b78d41ef54554a4
Patrick Perry <patperry@stanford.edu>
2009-05-04 18:16:03 -0700

    handle +/- zero specially for gamma=1 in dmp

--- R/mp.R
+++ R/mp.R
@@ -1,28 +1,32 @@
 
 dmp <- function( x, n.df=NA, p.dim=NA, var=1, concentration=n.df/p.dim, log = FALSE ) {
     gamma          <- concentration
+    
     inv.gamma.sqrt <- sqrt( 1/gamma )
     a              <- var*( 1 - inv.gamma.sqrt )^2
     b              <- var*( 1 + inv.gamma.sqrt )^2
     
     if( !log ) {
-        d <- ifelse( x <= a | x >= b, 0,
-                suppressWarnings(
-                    gamma/( 2*pi*var*x )
-                    *
-                    sqrt( ( x-a )*( b-x ) ) 
-                    ) )
+        # we have to handle +/- zero carefully when gamma=1
+        d <- ifelse( gamma == 1 & x == 0 & 1/x > 0, Inf, 
+                 ifelse( x <= a | x >= b, 0,
+                     suppressWarnings(
+                         gamma/( 2*pi*var*x )
+                         *
+                         sqrt( ( x-a )*( b-x ) ) 
+                         ) ) )
     } else {
-        d <- ifelse( x <= a | x >= b, -Inf,
-                 suppressWarnings(
-                     log( gamma ) 
-                     - 
-                     ( log( 2 ) + log( pi ) + log( var ) + log( x ) )
-                     +
-                     0.5*log( x-a ) 
-                     + 
-                     0.5*log( b-x )
-                     ) )
+        d <- ifelse( gamma == 1 & x == 0 & 1/x > 0, Inf,
+                 ifelse( x <= a | x >= b, -Inf,
+                     suppressWarnings(
+                         log( gamma ) 
+                         - 
+                         ( log( 2 ) + log( pi ) + log( var ) + log( x ) )
+                         +
+                         0.5*log( x-a ) 
+                         + 
+                         0.5*log( b-x )
+                         ) ) )
     }
     d
 }
